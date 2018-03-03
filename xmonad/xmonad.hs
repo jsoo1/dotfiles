f8b3fe19359891d0b6d7adca7d43fab0ed339178
John Soo <jsoo1@asu.edu>
2018-03-03 12:14:39 -0800

    XMonad looking shaaarp!

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,24 +1,53 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.Gaps
+import XMonad.Layout.Spacing
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 main :: IO ()
-main = do
-  xmobarPipe <- spawnPipe "xmobar"
+main =
+  do
+    xmobarPipe <- spawnPipe "/home/john/.local/bin/xmobar /home/john/.config/xmobar/xmobarrc"
+    xmonad $
+      docks
+        def
+          { terminal = "alacritty"
+          , focusFollowsMouse = False
+          , borderWidth = 3
+          , normalBorderColor = "#292b2e" -- dark grey
+          , focusedBorderColor = "#5d4d7a" -- purple
+          -- "#bc6ec5" -- pink -- "#2aa1ae" -- cyan -- "#5d4d7a" -- purple -- "#f4f4f4" -- off-white
 
-  xmonad $ def
-    { terminal = "alacritty"
-    , focusFollowsMouse = False
-    , manageHook = manageDocks <+> manageHook def
-    , layoutHook = avoidStruts $ layoutHook def
-    , handleEventHook = handleEventHook def <+> docksEventHook
-    , logHook = dynamicLogWithPP xmobarPP
-      { ppOutput = hPutStrLn xmobarPipe
-      , ppTitle = xmobarColor "green" "" . shorten 50
-      }
-    } `additionalKeys`
-    [  ((mod1Mask, xK_space), spawn "fish -c \"rofi -show combi -modi window,windowcd,ssh,drun,run\"")
-    ]
+          , layoutHook =
+              gaps [(U, 3)] $
+                smartSpacingWithEdge 14 $
+                  avoidStrutsOn [U, D] $
+                    layoutHook def
+
+          , manageHook =
+              manageDocks <+> manageHook def
+
+          , logHook =
+              dynamicLogWithPP
+                xmobarPP
+                  { ppOutput = hPutStrLn xmobarPipe
+                  , ppTitle = xmobarColor "green" "" . shorten 50
+                  }
+
+          , startupHook =
+              setWMName "LG3D"
+              <+> spawn "/usr/binxrandr --output HDMI-1-1 --primary --left-of eDP-1-1 --output eDP-1-1"
+              <+> spawn "/usr/bin/compton --config /home/john/.config/compton/compton.conf"
+              <+> spawn "/usr/bin/nitrogen --restore"
+              <+> spawn "/usr/bin/setxkbmap -layout us -option ctrl:nocaps"
+              <+> spawn "/usr/bin/xcape -e 'Control_L=Escape'"
+          }
+
+        `additionalKeys`
+          [ ((mod1Mask, xK_space), spawn "fish -c \"rofi -show combi -modi combi\"")
+          , ((mod1Mask .|. shiftMask, xK_X), spawn "sh /home/john/.i3/blurlock.sh")
+          ]
