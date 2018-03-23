f5d32b562cc6f5972ca460e72a289716d07c7457
John Soo <jsoo1@asu.edu>
2018-03-22 21:56:36 -0700

    Fix up guix conf.

--- guix/config.scm
+++ guix/config.scm
@@ -1,10 +1,5 @@
-;; This is an operating system configuration template
-;; for a "desktop" setup with GNOME and Xfce where the
-;; root partition is encrypted with LUKS.
-
 (use-modules (gnu) (gnu system nss))
 (use-service-modules desktop)
-;; (use-package-modules certs gnome)
 (use-package-modules bootloaders suckless xdisorg certs wm)
 
 (operating-system
@@ -12,8 +7,6 @@
   (timezone "America/Los_Angeles")
   (locale "en_US.utf8")
 
-  ;; Assuming /dev/sdX is the target hard disk, and "my-root"
-  ;; is the label of the target root file system.
   (bootloader (bootloader-configuration
                 (bootloader grub-efi-bootloader)
                 (target "/boot/efi")))
@@ -21,8 +14,8 @@
   (file-systems
     (cons*
       (file-system
-        (device "guix-root")
-	(title 'label)
+        (device "guix-large")
+        (title 'label)
         (mount-point "/")
         (type "ext4"))
       (file-system
@@ -43,17 +36,12 @@
                 (home-directory "/home/john"))
                %base-user-accounts))
 
-  ;; This is where we specify system-wide packages.
   (packages (cons* xmonad
                    dmenu
                    rofi
                    nss-certs         ;for HTTPS access
                    %base-packages))
 
-  ;; Add GNOME and/or Xfce---we can choose at the log-in
-  ;; screen with F1.  Use the "desktop" services, which
-  ;; include the X11 log-in service, networking with Wicd,
-  ;; and more.
   (services %desktop-services)
 
   ;; Allow resolution of '.local' host names with mDNS.
