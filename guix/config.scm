13cc63b50b6cb6d19c90d0b9e8ddccaf297e4243
John Soo <jsoo1@asu.edu>
2018-04-07 13:14:29 -0700

    Add a few more niceties to system install.

--- guix/config.scm
+++ guix/config.scm
@@ -1,5 +1,7 @@
-(use-modules (gnu) (gnu system nss))
-(use-service-modules desktop)
+(use-modules (gnu)
+             (gnu system nss)
+             (guix modules))
+(use-service-modules desktop ssh)
 (use-package-modules admin
                      bootloaders
                      certs
@@ -8,9 +10,12 @@
                      fonts
                      gnupg
                      image-viewers
+                     lsof
+                     ncurses
                      shells
                      ssh
                      suckless
+                     terminals
                      tmux
                      version-control
                      vim
@@ -20,81 +25,94 @@
                      xorg)
 
 (operating-system
-  (host-name "ecenter")
-  (timezone "America/Los_Angeles")
-  (locale "en_US.utf8")
+ (host-name "ecenter")
+ (timezone "America/Los_Angeles")
+ (locale "en_US.utf8")
 
-  (initrd-modules (append '(;; "ahcpi"
-	                       "shpchp")
-                          %base-initrd-modules))
+ (initrd-modules (append '(;; "ahcpi"
+                           "shpchp")
+                         %base-initrd-modules))
 
-  (bootloader (bootloader-configuration
-                (bootloader grub-efi-bootloader)
-                (target "/boot/efi")))
+ (bootloader (bootloader-configuration
+              (bootloader grub-efi-bootloader)
+              (target "/boot/efi")))
 
-  (file-systems (cons* (file-system
-                         (device "guix-root")
-                         (title 'label)
-                         (mount-point "/")
-                         (type "ext4"))
-                       (file-system
-                         (device (uuid "60E8-6B6F" 'fat))
-                         (title 'uuid)
-                         (mount-point "/boot/efi")
-                         (type "vfat"))
-                       %base-file-systems))
+ (file-systems (cons* (file-system
+                       (device "guix-root")
+                       (title 'label)
+                       (mount-point "/")
+                       (type "ext4"))
+                      (file-system
+                       (device (uuid "60E8-6B6F" 'fat))
+                       (title 'uuid)
+                       (mount-point "/boot/efi")
+                       (type "vfat"))
+                      %base-file-systems))
 
-  (users (cons (user-account
-                (name "john")
-                (comment "idiot man")
-                (group "users")
-                (supplementary-groups '("wheel"
-                                        "netdev"
-                                        "audio"
-                                        "video"
-                                        "lp"))
-                (home-directory "/home/john"))
-               %base-user-accounts))
+ (users (cons (user-account
+               (name "john")
+               (comment "idiot man")
+               (group "users")
+               (supplementary-groups '("wheel"
+                                       "netdev"
+                                       "audio"
+                                       "video"
+                                       "lp"))
+               (home-directory "/home/john")
+               (shell #~(string-append #$fish "/bin/fish")))
+              %base-user-accounts))
 
-  (packages (cons* ;; window manager related
-	           xmonad
-	           xmobar
-                   rofi
-		   dmenu
+ (packages (cons*
+            ;; window manager related
+            xmonad
+            xmobar
+            rofi
+            dmenu
 
-                   nss-certs                      ;for HTTPS access
+            ;;for HTTPS access
+            nss-certs
 
-                   ;; X related
-                   setxkbmap
-                   xrandr
-                   xfontsel
-                   mkfontdir
-                   mkfontscale
-                   compton
-                   xcape
-                   xclip
-                   feh
+            ;; X related
+            setxkbmap
+            xrandr
+            xfontsel
+            mkfontdir
+            mkfontscale
+            compton
+            xcape
+            xclip
+            feh
 
-                   ;; essentials
-                   git
-                   fish
-                   openssh
-                   gnupg
-                   htop
-                   tmux
+            ;; essentials
+            lsof
+            inetutils
+            git
+            fish
+            openssh
+            gnupg
+            htop
+            ncurses
+            tmux
 
-                   ;; text editors
-                   vim
-                   emacs
+            ;; text editors
+            vim
+            emacs
 
-                   ;; web browser
-                   qutebrowser
+            ;; no alacritty yet :(
+            termite
 
-                   font-adobe-source-code-pro
-                   font-fantasque-sans
-                   %base-packages))
+            ;; web browser
+            qutebrowser
 
-  (services %desktop-services)
+            font-adobe-source-code-pro
+            font-fantasque-sans
+            %base-packages))
 
-  ;; Allow resolution of '.local' host names with mDNS.
-  (name-service-switch %mdns-host-lookup-nss))
+ (services (cons*
+            ;; TODO: buy a libre bluetooth device
+            ;; (bluetooth-service #:auto-enable? #t)
+            (service openssh-service-type)
+            %desktop-services))
+
+ ;; Allow resolution of '.local' host names with mDNS.
+ (name-service-switch %mdns-host-lookup-nss))
