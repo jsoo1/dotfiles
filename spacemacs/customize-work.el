924b2ca99f11b9584710c17d65e1087f20ed722b
John Soo <jsoo@panosoft.com>
2018-06-18 09:34:38 -0700

    Add elm project safe projectile commands.

--- spacemacs/customize-work.el
+++ spacemacs/customize-work.el
@@ -3,9 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-want-Y-yank-to-eol nil)
  '(safe-local-variable-values
    (quote
     ((eval progn
+           (require
+            (quote projectile))
+           (puthash
+            (projectile-project-root)
+            "elm-make App.elm --output app.js" projectile-compilation-cmd-map)
+           (puthash
+            (projectile-project-root)
+            "node main.js" projectile-project-run-cmd))
+     (eval progn
            (require
             (quote projectile))
            (puthash
