7731cd5487290e061bfc2c20004d1965dfc7659a
John Soo <jsoo@panosoft.com>
2018-10-01 22:26:52 -0700

    Upstream changes

--- spacemacs/customize-work.el
+++ spacemacs/customize-work.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-want-Y-yank-to-eol nil)
  '(safe-local-variable-values
    (quote
     ((eval progn
@@ -10,7 +11,15 @@
             (quote projectile))
            (puthash
             (projectile-project-root)
-            "npm run watch" projectile-compilation-cmd-map)
+            "npm run build" projectile-compilation-cmd-map)
+           (setq projectile-project-run-cmd "npm start")
+           (setq projectile-project-test-cmd "npm run test"))
+     (eval progn
+           (require
+            (quote projectile))
+           (puthash
+            (projectile-project-root)
+            "npm run build" projectile-compilation-cmd-map)
            (setq projectile-project-run-cmd "npm start")
            (setq projectile-project-test-cmd "npm test"))
      (javascript-backend . tern)
