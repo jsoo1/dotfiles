7f86346ef5663b535110f6b17a9de3b82ab886a0
John Soo <jsoo1@asu.edu>
2018-11-24 08:30:37 -0800

    Use my xmobar with alsa, dbus support. WIP tamzen console font

--- guix/config.scm
+++ guix/config.scm
@@ -1,10 +1,6 @@
 (use-modules (gnu)
              (guix modules)
-             (guix packages)
-             (guix download)
-             (guix build-system)
-             (guix build-system haskell)
-             ((guix licenses) #:prefix license:)
+             (xmobar)
              (xmonad))
 (use-service-modules base
                      desktop
@@ -13,12 +9,9 @@
                      bootloaders
                      certs
                      commencement
-                     compton
                      emacs
                      fonts
                      gnupg
-                     haskell
-                     image-viewers
                      lsof
                      ncurses
                      shells
@@ -29,21 +22,16 @@
                      version-control
                      vim
                      wm
-                     xdisorg
-                     xorg)
-
+                     xdisorg)
 
 (operating-system
  (host-name "ecenter")
  (timezone "America/Los_Angeles")
  (locale "en_US.utf8")
-
  (initrd-modules %base-initrd-modules)
-
  (bootloader (bootloader-configuration
               (bootloader grub-efi-bootloader)
               (target "/boot/efi")))
-
  (file-systems (cons* (file-system
                        (device (uuid "462563db-3f82-44d2-829c-eb2bce9fd0e0" 'ext4))
                        (mount-point "/")
@@ -53,9 +41,7 @@
                        (mount-point "/boot/efi")
                        (type "vfat"))
                       %base-file-systems))
-
  (swap-devices '("/dev/sda7"))
-
  (users (cons (user-account
                (name "john")
                (comment "idiot man")
@@ -70,17 +56,16 @@
                ;; (shell #~(string-append #$fish "/bin/fish"))
                )
               %base-user-accounts))
-
  (packages (cons*
+            ;; nice tty font
+            font-tamzen
             ;; window manager related
-            ;; TODO: Use when supports alsa
             my-xmonad
             my-ghc-xmonad-contrib
+            xmobar-plus
             rofi
-
             ;;for HTTPS access
             nss-certs
-
             ;; essentials
             lsof
             inetutils
@@ -91,22 +76,25 @@
             htop
             ncurses
             tmux
-
             ;; text editors
             vim
             emacs
-
             %base-packages))
-
  (services (cons*
             ;; TODO: Add service for modprobe.d modules
-            ;; TODO: Build the terminus psf font from powerline
-            ;; (console-fonts "")
             (console-keymap-service "/home/john/dotfiles/minimal/Caps2Ctrl.map")
             (bluetooth-service #:auto-enable? #t)
             (service openssh-service-type)
-            %desktop-services))
-
+            ;; (kmscon-service-type (kmscon-configuration (virtual-terminal "tty3")))
+            (modify-services
+             %desktop-services
+             ('console-fonts s => '(("tty1" . "LatGrkCyr-8x16")
+                                    ("tty2" . "TamzenForPowerline8x16")
+                                    ;; ("tty3" . "LatGrkCyr-8x16")
+                                    ("tty4" . "LatGrkCyr-8x16")
+                                    ("tty5" . "LatGrkCyr-8x16")
+                                    ("tty6" . "LatGrkCyr-8x16")))
+             ('kmscon c => (kmscon-configuration (virtual-terminal "tty3"))))))
  ;; Allow resolution of '.local' host names with mDNS.
  (name-service-switch %mdns-host-lookup-nss))
 
