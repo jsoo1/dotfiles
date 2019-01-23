ea8c5faaaf7d31b610cece4b1c9afbdd171dabbb
John Soo <jsoo1@asu.edu>
2019-01-23 08:25:08 -0800

    Format config.scm

--- guix/config.scm
+++ guix/config.scm
@@ -57,113 +57,119 @@ EndSection")
   (timezone "America/Los_Angeles")
   (locale "en_US.utf8")
   (initrd-modules %base-initrd-modules)
-  (bootloader (bootloader-configuration
-               (bootloader grub-efi-bootloader)
-               (target "/boot/efi")
-               (menu-entries `(,(menu-entry
-                                 (label "ubuntu")
-                                 (linux "/boot/vmlinuz-4.15.0-43-generic")
-                                 (linux-arguments '("root=UUID=45658f04-8790-437f-9590-13025ffb7264"
-                                                    "ro"
-                                                    "quiet"
-                                                    "splash"
-                                                    "vt.handoff=1"))
-                                 (initrd "/boot/initrd.img-4.15.0-43-generic"))))))
-  (file-systems (cons* (file-system
-                         (device (uuid "462563db-3f82-44d2-829c-eb2bce9fd0e0" 'ext4))
-                         (mount-point "/")
-                         (type "ext4"))
-                       (file-system
-                         (device (uuid "60E8-6B6F" 'fat))
-                         (mount-point "/boot/efi")
-                         (type "vfat"))
-                       %base-file-systems))
+  (bootloader
+   (bootloader-configuration
+    (bootloader grub-efi-bootloader)
+    (target "/boot/efi")
+    (menu-entries
+     `(,(menu-entry
+         (label "ubuntu")
+         (linux "/boot/vmlinuz-4.15.0-43-generic")
+         (linux-arguments
+          '("root=UUID=45658f04-8790-437f-9590-13025ffb7264"
+            "ro"
+            "quiet"
+            "splash"
+            "vt.handoff=1"))
+         (initrd "/boot/initrd.img-4.15.0-43-generic"))))))
+  (file-systems
+   (cons* (file-system
+            (device (uuid "462563db-3f82-44d2-829c-eb2bce9fd0e0" 'ext4))
+            (mount-point "/")
+            (type "ext4"))
+          (file-system
+            (device (uuid "60E8-6B6F" 'fat))
+            (mount-point "/boot/efi")
+            (type "vfat"))
+          %base-file-systems))
   (swap-devices '("/dev/sda7"))
-  (users (cons (user-account
-                (name "john")
-                (comment "idiot man")
-                (group "users")
-                (supplementary-groups '("wheel"
-                                        "netdev"
-                                        "audio"
-                                        "video"
-                                        "lp"))
-                (home-directory "/home/john")
-                (shell #~(string-append #$fish "/bin/fish")))
-               %base-user-accounts))
-  (packages (cons*
-             ;; nice tty font
-             font-tamzen
-             ;; kmscon fonts
-             fontconfig
-             font-adobe-source-code-pro
-             font-fantasque-sans
-             font-iosevka
-             ;; window manager related
-             my-xmonad
-             my-ghc-xmonad-contrib
-             xmobar-plus
-             rofi
-             ;;for HTTPS access
-             curl
-             nss-certs
-             ;; essentials
-             lsof
-             inetutils
-             git
-             fish
-             openssh
-             gnupg
-             htop
-             ncurses
-             tmux
-             glibc-utf8-locales
-             ;; text editors
-             vim
-             emacs
-             %base-packages))
-  (services (cons*
-             ;; TODO: Add service for modprobe.d modules?
-             (bluetooth-service #:auto-enable? #t)
-             (console-keymap-service "/home/john/dotfiles/minimal/Caps2Ctrl.map")
-             (service xcape-service-type '(("Control_L" . "Escape")))
-             (service kmscon-service-type
-                      (kmscon-configuration
-                       (virtual-terminal "tty8")
-                       (scrollback "100000")
-                       (font-name "'Fantasque Sans Mono'")
-                       (font-size "15")
-                       (xkb-layout "us")
-                       (xkb-variant "")
-                       (xkb-options "ctrl:nocaps")))
-             (service openssh-service-type)
-             (service thermald-service-type (thermald-configuration))
-             (service tlp-service-type
-                      (tlp-configuration
-                       (tlp-default-mode "BAT")
-                       (usb-autosuspend? #f)))
-             (modify-services
-                 %desktop-services
-               ('console-font-service-type
-                s =>
-                (list
-                 '("tty1" "LatGrkCyr-8x16")
-                 '("tty2" "TamzenForPowerline8x16")
-                 '("tty3" "LatGrkCyr-8x16")
-                 '("tty4" "LatGrkCyr-8x16")
-                 '("tty5" "LatGrkCyr-8x16")
-                 '("tty6" "LatGrkCyr-8x16")))
-               ('slim-service-type
-                c =>
-                (slim-configuration
-                 (inherit c)
-                 (startx (xorg-start-command
-                          #:configuration-file
-                          (xorg-configuration-file
-                           #:extra-config
-                           (list
-                            cst-trackball
-                            caps2control)))))))))
+  (users
+   (cons (user-account
+          (name "john")
+          (comment "idiot man")
+          (group "users")
+          (supplementary-groups
+           '("wheel"
+             "netdev"
+             "audio"
+             "video"
+             "lp"))
+          (home-directory "/home/john")
+          (shell #~(string-append #$fish "/bin/fish")))
+         %base-user-accounts))
+  (packages
+   (cons*
+    ;; nice tty font
+    font-tamzen
+    ;; kmscon fonts
+    fontconfig
+    font-adobe-source-code-pro
+    font-fantasque-sans
+    font-iosevka
+    ;; window manager related
+    my-xmonad
+    my-ghc-xmonad-contrib
+    xmobar-plus
+    rofi
+    ;;for HTTPS access
+    curl
+    nss-certs
+    ;; essentials
+    lsof
+    inetutils
+    git
+    fish
+    openssh
+    gnupg
+    htop
+    ncurses
+    tmux
+    glibc-utf8-locales
+    ;; text editors
+    vim
+    emacs
+    %base-packages))
+  (services
+   (cons*
+    ;; TODO: Add service for modprobe.d modules?
+    (bluetooth-service #:auto-enable? #t)
+    (console-keymap-service "/home/john/dotfiles/minimal/Caps2Ctrl.map")
+    (service xcape-service-type '(("Control_L" . "Escape")))
+    (service kmscon-service-type
+             (kmscon-configuration
+              (virtual-terminal "tty8")
+              (scrollback "100000")
+              (font-name "'Fantasque Sans Mono'")
+              (font-size "15")
+              (xkb-layout "us")
+              (xkb-variant "")
+              (xkb-options "ctrl:nocaps")))
+    (service openssh-service-type)
+    (service thermald-service-type (thermald-configuration))
+    (service tlp-service-type
+             (tlp-configuration
+              (tlp-default-mode "BAT")
+              (usb-autosuspend? #f)))
+    (modify-services %desktop-services
+      ('console-font-service-type
+       s =>
+       (list
+        '("tty1" "LatGrkCyr-8x16")
+        '("tty2" "TamzenForPowerline8x16")
+        '("tty3" "LatGrkCyr-8x16")
+        '("tty4" "LatGrkCyr-8x16")
+        '("tty5" "LatGrkCyr-8x16")
+        '("tty6" "LatGrkCyr-8x16")))
+      ('slim-service-type
+       c =>
+       (slim-configuration
+        (inherit c)
+        (startx
+         (xorg-start-command
+          #:configuration-file (xorg-configuration-file
+                                #:extra-config (list
+                                                cst-trackball
+                                                caps2control)))))))))
   ;; Allow resolution of '.local' host names with mDNS.
   (name-service-switch %mdns-host-lookup-nss))
 
