04c73ee34cdd9cb5364c5fde1fdbcb487560fdc6
John Soo <jsoo1@asu.edu>
2019-02-10 07:49:07 -0800

    Remove host from status

--- xmobar/xmobar.hs
+++ xmobar/xmobar.hs
@@ -59,16 +59,16 @@ Config
       "default"
       "Master"
       [ "-t"
-      , " <volume>%  <status>  vol "
+      , "<volume>%  <status>  vol"
       , "--"
       , "--on"
       , "on"
       , "--onc"
-      , "#586e75,#002b36"
+      , "#002b36,#586e75"
       , "-o"
       , "off"
       , "--offc"
-      , "#dc322f,#002b36"
+      , "#002b36,#dc322f"
       ]
       200
     , Run Date "%Y-%m-%d  %l:%M %p" "date" 600
@@ -77,11 +77,10 @@ Config
   , sepChar = "%"
   , alignSep = "}{"
   , template =
-    "<fc=#002b36,#859900> workspaces </fc><fc=#859900,#002b36></fc>%StdinReader%\
+    "<fc=#002b36,#859900> hi </fc><fc=#859900,#002b36></fc>%StdinReader%\
       \}{\
-      \<action=`amixer -q set Master toggle`><fc=#002b36,#002b36></fc><fc=#586e75,#002b36>%default:Master%</fc></action>\
-      \<fc=#586e75,#002b36></fc><fc=#002b36,#586e75> %date% </fc><fc=#002b36,#586e75></fc>\
+      \<action=`amixer -q set Master toggle`><fc=#586e75,#002b36></fc><fc=#002b36,#586e75> %default:Master% </fc></action><fc=#002b36,#586e75></fc>\
       \<fc=#002b36,#002b36></fc><fc=#586e75,#002b36>%wlp9s0wi% </fc>\
       \<fc=#586e75,#002b36> bat %battery%</fc><fc=#002b36,#002b36></fc>\
-      \<fc=#586e75,#002b36></fc><fc=#002b36,#586e75> %hostname% </fc>"
+      \<fc=#586e75,#002b36></fc><fc=#002b36,#586e75> %date% </fc>"
   }
