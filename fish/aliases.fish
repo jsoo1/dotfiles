926df66b8ecacafd358f3abbe5f17310f515b073
John Soo <jsoo1@asu.edu>
2019-03-19 12:59:43 -0700

    Use new abbr features

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,39 +1,37 @@
 # Git
-abbr gb 'git branch'
-abbr gbd 'git branch -D'
-abbr gst 'git status'
-abbr gf 'git fetch'
-abbr ga 'git add'
-abbr gc 'git commit'
-abbr gcb 'git checkout -b'
-abbr glg 'git log'
-abbr grb 'git rebase'
-abbr gl 'git pull'
-abbr gp 'git push'
-abbr gd 'git diff'
-abbr gm 'git merge'
-abbr gco 'git checkout'
+abbr --add -U -- gb 'git branch'
+abbr --add -U -- gst 'git status'
+abbr --add -U -- gf 'git fetch'
+abbr --add -U -- ga 'git add'
+abbr --add -U -- gc 'git commit'
+abbr --add -U -- glg 'git log'
+abbr --add -U -- grb 'git rebase'
+abbr --add -U -- gl 'git pull'
+abbr --add -U -- gp 'git push'
+abbr --add -U -- gd 'git diff'
+abbr --add -U -- gm 'git merge'
+abbr --add -U -- gco 'git checkout'
 alias gitpurge 'git branch --merged | grep -v "\*" | grep -v "master" | xargs -n 1 git branch -d'
 
 # System Utils
-abbr lsa "ls -lsa"
-abbr lsah "ls -lsah"
-abbr psg 'ps -eF | grep -i'
-abbr rest "loginctl suspend"
-abbr bat "upower -I (upower -e | grep BAT)"
+abbr --add -U -- lsa "ls -lsa"
+abbr --add -U -- lsah "ls -lsah"
+abbr --add -U -- psg 'ps -eF | grep -i'
+abbr --add -U -- rest "loginctl suspend"
+abbr --add -U -- bat "upower -I (upower -e | grep BAT)"
 
 # Emacs
-abbr ed "emacs -q -l ~/dotfiles/emacs/init.el --bg-daemon=term"
-abbr em "env TERM=xterm-24bits emacsclient -nw --socket-name term"
+abbr --add -U -- ed "emacs -q -l ~/dotfiles/emacs/init.el --bg-daemon=term"
+abbr --add -U -- em "env TERM=xterm-24bits emacsclient -nw --socket-name term"
 # TODO Figure out fish issues
-abbr e "env TERM=xterm-24bits emacsclient -nw --socket-name term"
+abbr --add -U -- e "env TERM=xterm-24bits emacsclient -nw --socket-name term"
 
 # Tmux
-abbr tma "env TERM=xterm-24bits tmux attach -t"
-abbr ta "env TERM=xterm-24bits tmux attach -t"
-abbr tml "tmux list-sessions"
-abbr tmux "env TERM=xterm-24bits tmux new-session -A -s (basename (pwd)) -n emacs"
-abbr tm "env TERM=xterm-24bits tmux new-session -A -s (basename (pwd)) -n emacs"
+abbr --add -U -- tma "env TERM=xterm-24bits tmux attach -t"
+abbr --add -U -- ta "env TERM=xterm-24bits tmux attach -t"
+abbr --add -U -- tml "tmux list-sessions"
+abbr --add -U -- tmux "env TERM=xterm-24bits tmux new-session -A -s (basename (pwd)) -n emacs"
+abbr --add -U -- tm "env TERM=xterm-24bits tmux new-session -A -s (basename (pwd)) -n emacs"
 
 # Lynx
 abbr lynx = lynx -cfg=~/.config/lynx/lynx.cfg
