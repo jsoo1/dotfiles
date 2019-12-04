4e6bf818b56cee0bb0e238f88a7d1696f66e8ebd
John Soo <jsoo1@asu.edu>
2019-12-04 08:06:22 -0800

    The great fish cleanup.

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -11,28 +11,31 @@ abbr --add -U -- gp 'git push'
 abbr --add -U -- gd 'git diff'
 abbr --add -U -- gm 'git merge'
 abbr --add -U -- gco 'git checkout'
-alias gitpurge 'git branch --merged | grep -v "\*" | grep -v "master" | xargs -n 1 git branch -d'
+function gitpurge
+    git branch --merged | rg -v "\*" | rg -v "master" | xargs -n 1 git branch -d
+end
 
 # System Utils
 abbr --add -U -- lsa "ls -lsa"
 abbr --add -U -- lsah "ls -lsah"
-abbr --add -U -- psg 'ps -eF | grep -i'
+abbr --add -U -- psg 'ps -eF | rg -i'
 abbr --add -U -- rest "loginctl suspend"
-abbr --add -U -- bat "upower -I (upower -e | grep BAT)"
+abbr --add -U -- bat "upower -I (upower -e | rg BAT)"
 
 # Emacs
 abbr --add -U -- ed "emacs --bg-daemon=term"
 function em
-    env TERM=xterm-24bits emacsclient -nw --socket-name term
+    env TERM=xterm-24bits emacsclient -nw --socket-name term $argv
 end
+abbr --add -U -- e env TERM=xterm-24bits emacsclient -nw --socket-name term $argv
 
 # Tmux
+abbr --add -U -- tmux "env TERM=xterm-24bits tmux new-session -A -s (basename (pwd)) -n emacs"
 function tm
     env TERM=xterm-24bits tmux new-session -A -s (basename (pwd)) -n emacs
 end
 abbr --add -U -- tma "env TERM=xterm-24bits tmux attach -t"
 abbr --add -U -- tml "tmux list-sessions"
-abbr --add -U -- tmux "env TERM=xterm-24bits tmux new-session -A -s (basename (pwd)) -n emacs"
 
 # Lynx
 abbr --add -U -- lynx lynx -cfg=~/.config/lynx/lynx.cfg
