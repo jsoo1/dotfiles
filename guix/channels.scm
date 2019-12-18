0c70b7aa0efe5dc21e47fd5226aa3a1c4a2ffcf9
John Soo <jsoo1@asu.edu>
2019-12-18 07:06:18 -0800

    Use master.

--- guix/channels.scm
+++ guix/channels.scm
@@ -2,7 +2,7 @@
  (channel
   (name 'guix)
   (url "file:///home/john/projects/guix/.git")
-  (branch "delete-extra-lib-dirs"))
+  (branch "master"))
  (channel
   (name 'private)
   (url "file:///home/john/projects/guix-channel/.git")
