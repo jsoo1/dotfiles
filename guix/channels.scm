6b4a7cd9b4c5e84d1d2a8653081a8fc1ce42b3be
John Soo <jsoo1@asu.edu>
2019-12-29 23:58:49 -0800

    Let's not make the mistake of updating to master again.

--- guix/channels.scm
+++ guix/channels.scm
@@ -2,7 +2,7 @@
  (channel
   (name 'guix)
   (url "file:///home/john/projects/guix/.git")
-  (branch "master"))
+  (branch "my-master"))
  (channel
   (name 'private)
   (url "file:///home/john/projects/guix-channel/.git")
