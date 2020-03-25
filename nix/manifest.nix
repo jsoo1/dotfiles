3679e887200e6cd1a1a72d2b1ea07d8829f3873e
John Soo <jsoo@panosoft.com>
2020-03-24 17:10:05 -0700

    Replace some more packages with nix.

--- nix/manifest.nix
+++ nix/manifest.nix
@@ -1,22 +1,33 @@
 let pkgs = import <nixpkgs> { };
 
 in [
+  pkgs.bash
+  pkgs.bash-completion
   pkgs.checkmake
+  pkgs.coq
   pkgs.direnv
+  pkgs.exa
   pkgs.fd
+  pkgs.fish
   pkgs.fzy
+  pkgs.ghcid
   pkgs.global
+  pkgs.hlint
+  pkgs.hoogle
   pkgs.htop
-  pkgs.idris
   pkgs.jq
   pkgs.ngrok
   pkgs.nixfmt
   pkgs.nodejs-10_x
+  pkgs.openvpn
+  pkgs.ormolu
+  pkgs.pijul
   pkgs.postgresql_11
   pkgs.qemu
   pkgs.racket-minimal
   pkgs.ripgrep
   pkgs.rustfmt
+  pkgs.stylish-haskell
   pkgs.tmux
   pkgs.watch
 ]
