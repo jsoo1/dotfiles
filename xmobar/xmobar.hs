f5818d6cf6ff3aa2a899506542cbd67d4037e294
John Soo <jsoo1@asu.edu>
2020-03-27 17:33:52 -0700

    Use light font style on smaller laptop screen.

--- xmobar/xmobar.hs
+++ xmobar/xmobar.hs
@@ -1,6 +1,6 @@
 Config
   { font =
-    "xft:Iosevka Term Slab:size=15:normal:antialias=true"
+    "xft:Iosevka Term Slab:size=15:light:antialias=true"
   , additionalFonts = []
   , borderColor = "#002b36"
   , border = TopB
