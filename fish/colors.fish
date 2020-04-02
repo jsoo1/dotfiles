51cd7e854fabd233dc084849238f10b8c8c1ea97
John Soo <jsoo1@asu.edu>
2020-04-01 22:36:30 -0700

    Nicer solarized.

--- fish/colors.fish
+++ fish/colors.fish
@@ -1,65 +1,32 @@
-# http://ethanschoonover.com/solarized#the-values
-
-# Use these settings if you've applied a Solarized theme to your terminal (for
-# example, if "ls -G" produces Solarized output). i.e. if "blue" is #268bd2, not
-# whatever the default is. (See "../etc/Solarized Dark.terminal" for OS X.)
-
-set -l base03  "--bold black"
-set -l base02  "black"
-set -l base01  "--bold green"
-set -l base00  "--bold yellow"
-set -l base0   "--bold blue"
-set -l base1   "--bold cyan"
-set -l base2   "white"
-set -l base3   "--bold white"
-set -l yellow  "yellow"
-set -l orange  "--bold red"
-set -l red     "red"
-set -l magenta "magenta"
-set -l violet  "--bold magenta"
-set -l blue    "blue"
-set -l cyan    "cyan"
-set -l green   "green"
-
-# Use these settings if your terminal supports term256 and your terminal hasn't
-# been configured with a Solarized theme.i.e. if "blue" is the default blue, not
-# Solarized blue.
-
-# set -l base03  002b36
-# set -l base02  073642
-# set -l base01  586e75
-# set -l base00  657b83
-# set -l base0   839496
-# set -l base1   93a1a1
-# set -l base2   eee8d5
-# set -l base3   fdf6e3
-# set -l yellow  b58900
-# set -l orange  cb4b16
-# set -l red     dc322f
-# set -l magenta d33682
-# set -l violet  6c71c4
-# set -l blue    268bd2
-# set -l cyan    2aa198
-# set -l green   859900
-
-# Used by fish's completion; see
-# http://fishshell.com/docs/2.0/index.html#variables-color
-
-set -g fish_color_normal      $base0
-set -g fish_color_command     $base0
-set -g fish_color_quote       $cyan
-set -g fish_color_redirection $base0
-set -g fish_color_end         $base0
-set -g fish_color_error       $red
-set -g fish_color_param       $blue
-set -g fish_color_comment     $base01
-set -g fish_color_match       $cyan
-set -g fish_color_search_match "--background=$base02"
-set -g fish_color_operator    $orange
-set -g fish_color_escape      $cyan
-
-# Used by fish_prompt
-
-set -g fish_color_hostname    $cyan
-set -g fish_color_cwd         $yellow
-set -g fish_color_git $green
+SET __fish_init_2_39_8:\x1d
+SET __fish_init_2_3_0:\x1d
+SET fish_color_autosuggestion:555\x1ebrblack
+SET fish_color_cancel:\x2dr
+SET fish_color_command:\x2d\x2dbold
+SET fish_color_comment:red
+SET_EXPORT fish_color_cwd:magenta
+SET fish_color_cwd_root:red
+SET fish_color_end:brmagenta
+SET fish_color_error:brred
+SET fish_color_escape:bryellow\x1e\x2d\x2dbold
+SET fish_color_history_current:\x2d\x2dbold
+SET fish_color_host:normal
+SET fish_color_match:\x2d\x2dbackground\x3dbrblue
+SET fish_color_normal:normal
+SET fish_color_operator:bryellow
+SET fish_color_param:cyan
+SET fish_color_quote:yellow
+SET fish_color_redirection:brblue
+SET fish_color_search_match:bryellow\x1e\x2d\x2dbackground\x3dbrblack
+SET fish_color_selection:white\x1e\x2d\x2dbold\x1e\x2d\x2dbackground\x3dbrblack
+SET fish_color_user:brgreen
+SET fish_color_valid_path:\x2d\x2dunderline
+SET fish_greeting:
+SET fish_key_bindings:fish_vi_key_bindings
+SET fish_pager_color_completion:\x1d
+SET fish_pager_color_description:B3A06D\x1eyellow
+SET fish_pager_color_prefix:white\x1e\x2d\x2dbold\x1e\x2d\x2dunderline
+SET fish_pager_color_progress:brwhite\x1e\x2d\x2dbackground\x3dcyan
+SET fish_user_abbreviations:gb\x20git\x20branch\x1egst\x20git\x20status\x1egf\x20git\x20fetch\x1ega\x20git\x20add\x1egc\x20git\x20commit\x1eglg\x20git\x20log\x1egrb\x20git\x20rebase\x1egl\x20git\x20pull\x1egp\x20git\x20push\x1egd\x20git\x20diff\x1egm\x20git\x20merge\x1egco\x20git\x20checkout\x1elsa\x20ls\x20\x2dlsa\x1elsah\x20ls\x20\x2dlsah\x1ermi\x20rm\x20\x2di\x1erest\x20loginctl\x20suspend\x1elogout\x20gnome\x2dsession\x2dquit\x20\x2d\x2dlogout\x20\x2d\x2dno\x2dprompt\x1eupgrade\x21\x20sudo\x20apt\x2dget\x20update\x3b\x20sudo\x20apt\x2dget\x20\x2dy\x20upgrade\x1einstall\x21\x20sudo\x20apt\x2dget\x20update\x3b\x20sudo\x20apt\x2dget\x20install\x1ectl\x20systemctl\x1ectlu\x20systemctl\x20\x2d\x2duser\x1ectllint\x20systemd\x2danalyze\x20verify\x1emindex\x20mu\x20index\x20\x2d\x2dmaildir\x20\x7e/\x2email\x1etml\x20tmux\x20list\x2dsessions\x1epsg\x20ps\x20\x2deF\x20\x7c\x20grep\x20\x2di\x1ebat\x20upower\x20\x2dI\x20\x28upower\x20\x2de\x20\x7c\x20grep\x20BAT\x29\x1eed\x20emacs\x20\x2dq\x20\x2dl\x20\x7e/dotfiles/emacs/init\x2eel\x20\x2d\x2dbg\x2ddaemon\x3dterm\x1eem\x20env\x20TERM\x3dxterm\x2d24bits\x20emacsclient\x20\x2dnw\x20\x2d\x2dsocket\x2dname\x20term\x1etmux\x20env\x20TERM\x3dxterm\x2d24bits\x20tmux\x20new\x2dsession\x20\x2dA\x20\x2ds\x20\x28basename\x20\x28pwd\x29\x29\x20\x2dn\x20emacs\x1elynx\x20\x3d\x20lynx\x20\x2dcfg\x3d\x7e/\x2econfig/lynx/lynx\x2ecfg\x1ee\x20env\x20TERM\x3dxterm\x2d24bits\x20emacsclient\x20\x2dnw\x20\x2d\x2dsocket\x2dname\x20term\x1etma\x20env\x20TERM\x3dxterm\x2d24bits\x20tmux\x20attach\x20\x2dt\x1eta\x20env\x20TERM\x3dxterm\x2d24bits\x20tmux\x20attach\x20\x2dt\x1etm\x20env\x20TERM\x3dxterm\x2d24bits\x20tmux\x20new\x2dsession\x20\x2dA\x20\x2ds\x20\x28basename\x20\x28pwd\x29\x29\x20\x2dn\x20emacs
+SET fisher_active_prompt:taktoa
+SET fisher_dependency_count:taktoa
