60ae5e26a8f40c64e5f56af0fc715f3ed7e75735
John Soo <jsoo1@asu.edu>
2020-04-05 21:24:43 -0700

    Let's try not logging out immediately.

--- fish/config.fish
+++ fish/config.fish
@@ -66,6 +66,6 @@ and eval (direnv hook fish)
 # eval (opam env)
 
 if test (tty) = /dev/tty1
-    xinit ~/.xsession -- /run/setuid-programs/*-startx
-    loginctl terminate-session (loginctl list-sessions | gawk '/tty1/ { print $1 }')
+    xinit ~/.xsession -- /run/setuid-programs/X vt1
+    # loginctl terminate-session (loginctl list-sessions | gawk '/tty1/ { print $1 }')
 end
