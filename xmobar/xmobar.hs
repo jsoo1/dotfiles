2b8dd2f919b6af7eee7cd5a45d669df737054cbd
John Soo <jsoo1@asu.edu>
2020-04-08 16:08:23 -0700

    Show backlight level in bar, use event based volume.
    * Cleanup unused bar elements.

--- xmobar/xmobar.hs
+++ xmobar/xmobar.hs
@@ -16,29 +16,12 @@ Config
   , hideOnStart = False
   , iconRoot = "."
   , commands =
-    [ Run Weather
-        "KSNA"
-        [ "-t"
-        , "<tempF>°F"
-        , "-L"
-        , "65"
-        , "-H"
-        , "85"
-        , "--normal"
-        , "green,#859900"
-        , "--high"
-        , "red,#cb4b16"
-        , "--low"
-        , "lightblue,#2aa198"
-        ]
-        9000
-    , Run Wireless
+    [ Run Wireless
       "wlp9s0"
       [ "-t"
       , "<essid> <quality>%"
       ]
       200
-    , Run Com "hostname" [] "hostname" 0
     , Run Battery
         [ "-t"
         , "<left>%"
@@ -53,7 +36,7 @@ Config
         , "#dc322f"
         ]
         200
-    , Run Volume
+    , Run Alsa
       "default"
       "Master"
       [ "-t"
@@ -62,23 +45,24 @@ Config
       , "--on"
       , "on"
       , "--onc"
-      , "#002b36,#586e75"
+      , "#586e75,#002b36"
       , "-o"
       , "off"
       , "--offc"
       , "#002b36,#dc322f"
       ]
-      200
     , Run Date "%Y-%m-%d | %l:%M %p " "date" 600
     , Run UnsafeStdinReader
+    , Run Com "light" [] "light" 2
     ]
   , sepChar = "%"
   , alignSep = "}{"
   , template =
     "<fc=#002b36,#859900> λ</fc>%UnsafeStdinReader%\
       \}{\
-      \<action=`amixer -q set Master toggle`><fc=#002b36,#586e75> %default:Master% </fc></action>\
-      \<fc=#586e75,#002b36> %wlp9s0wi% </fc>\
-      \<fc=#586e75,#002b36>| bat %battery% </fc>\
-      \<fc=#002b36,#586e75> %date% </fc>"
+      \<fc=#002b36,#586e75> light %light% </fc>\
+      \<action=`amixer -q set Master toggle`><fc=#586e75,#002b36> %alsa:default:Master% </fc></action>\
+      \<fc=#002b36,#586e75> %wlp9s0wi% </fc>\
+      \<fc=#002b36,#586e75>| bat %battery% </fc>\
+      \<fc=#586e75,#002b36> %date% </fc>"
   }
