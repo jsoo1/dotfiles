6680c64390efc1d5f2accb3144c60fe2b0bab508
John Soo <jsoo1@asu.edu>
2020-12-17 09:11:43 -0800

    Fix evil tab-switching keybindings in dired.

--- emacs/init.el
+++ emacs/init.el
@@ -383,6 +383,13 @@
 (evil-declare-not-repeat #'flycheck-next-error)
 (evil-declare-not-repeat #'flycheck-previous-error)
 
+;; Evil tab motions in dired
+(add-hook 'dired-mode-hook
+          (defun fix-dired-tab-motions ()
+            (interactive)
+            (evil-local-set-key 'normal (kbd "g t") #'tab-bar-switch-to-next-tab)
+            (evil-local-set-key 'normal (kbd "g T") #'tab-bar-switch-to-prev-tab)))
+
 ;; Vinegar
 (define-key evil-normal-state-map "-" (defun dired-dot () (interactive) (dired ".")))
 (define-key dired-mode-map "-" #'dired-up-directory)
