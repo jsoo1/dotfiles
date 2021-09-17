aad34de96ba1768cd5989fef056eaad2dc59967b
John Soo <jsoo1@asu.edu>
2021-09-17 12:20:54 -0700

    Use release branch and channel introduction for my channel.

--- guix/channels.scm
+++ guix/channels.scm
@@ -6,4 +6,9 @@
  (channel
   (name 'private)
   (url "file:///home/john/projects/guix-channel/.git")
-  (branch "upstream-rustfmt")))
+  (branch "release")
+  (introduction
+   (make-channel-introduction
+    "cf22e20eceb112c940d02c3b57e16c7cee1f4a7f"
+    (openpgp-fingerprint
+     "5A15 8FAF 406A 748A 81A9  DC4E 4F43 7A76 B448 A23B")))))
