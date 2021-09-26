e2c041d6c583928a3e41c4837ecca76d61f40506
John Soo <jsoo1@asu.edu>
2021-09-25 17:08:06 -0700

    tealdeer successfully upstreamed.

--- guix/channels.scm
+++ guix/channels.scm
@@ -5,13 +5,13 @@
   (branch "john")
   (introduction
    (make-channel-introduction
-    "a477f10d42f31c50aa63ad79ba6d2e23248cdcdc"
+    "76ce6318d8c3da0bfd3d41d0525c811bd9820090"
     (openpgp-fingerprint
      "5A15 8FAF 406A 748A 81A9  DC4E 4F43 7A76 B448 A23B"))))
  (channel
   (name 'private)
   (url "file:///home/john/projects/guix-channel/.git")
-  (branch "upstream-tealdeer")
+  (branch "release")
   (introduction
    (make-channel-introduction
     "cf22e20eceb112c940d02c3b57e16c7cee1f4a7f"
