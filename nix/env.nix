08f5df6bddcd9a7608b8dc23c03260cbf2b26191
John Soo <jsoo1@asu.edu>
2021-10-02 09:34:10 -0700

    organize nix env.

--- nix/env.nix
+++ nix/env.nix
@@ -1,16 +1,16 @@
-let pkgs = import ../pin.nix;
-in with pkgs; [
-  direnv
-  fd
-  ghcid
-  gnupg
-  iosevka
-  mosh
-  my-emacs
-  neovim
-  restream
-  ripgrep
-  rnix-lsp
-  skim
-  tmux
+with import ../pin.nix;
+let
+  emacs = [ my-emacs pinentry-emacs ];
+  fonts = [ iosevka ];
+  haskell-utilities = [ ghcid haskell-language-server ];
+  nix-utilities = [ rnix-lsp ];
+  remarkable-utilities = [ restream ];
+  shell-utilities = [ direnv fd git gnupg mosh neovim ripgrep skim tmux ];
+in builtins.concatLists [
+  emacs
+  fonts
+  haskell-utilities
+  nix-utilities
+  remarkable-utilities
+  shell-utilities
 ]
