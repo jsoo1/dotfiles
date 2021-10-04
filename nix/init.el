4a8aa63efbda3bc1e4b75b0f992762a8e2882807
John Soo <jsoo1@asu.edu>
2021-10-04 16:47:33 -0700

    nix: Fix projectile globally unignored files and directories.

--- nix/init.el
+++ nix/init.el
@@ -511,10 +511,11 @@
 (require 'projectile)
 (projectile-mode +1)
 (setq projectile-completion-system 'ivy
-      projectile-indexing-method 'hybrid
+      projectile-indexing-method 'native
       projectile-enable-caching 't
       projectile-project-search-path "~/projects/"
       projectile-globally-unignored-files '(".*\\.projectile$" ".*\\.envrc$" ".*\\.dir-locals.el$")
+      projectile-globally-unignored-directories '("^/scratch/.*")
       projectile-project-root-files-functions (list #'projectile-root-local
                                                     #'projectile-root-top-down-recurring
                                                     #'projectile-root-top-down
