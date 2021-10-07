857770ec882fafb6b7d355ac9444b155649277a7
John Soo <john.soo@arista.com>
2021-10-07 09:07:25 -0700

    nix: Keep darwin things out of linux.

--- nix/home.nix
+++ nix/home.nix
@@ -16,16 +16,15 @@ let
       shell-utilities = [ bat dogdns fd git jq mosh ripgrep ];
     in builtins.concatLists [
       emacs
-      fonts
       haskell-utilities
       nix-utilities
-      remarkable-utilities
+      (if builtins.currentSystem == "x86_64-darwin" then
+        fonts ++ remarkable-utilities
+      else
+        [ ])
       shell-utilities
     ];
-  file = {
-    ".vimrc" = { source = "${dotfiles}/minimal/.vimrc"; };
-    ".tmux.conf" = { source = "${dotfiles}/minimal/.tmux.conf"; };
-    ".tmuxline.conf" = { source = "${dotfiles}/minimal/.tmuxline.conf"; };
+  emacs-files = {
     ".emacs.d/${bq-opml}" = {
       source = "${dotfiles}/${bq-opml}";
       target = ".emacs.d/${bq-opml}";
@@ -35,61 +34,58 @@ let
       target = ".emacs.d/${downcast-opml}";
     };
   };
-  sk = "${pkgs.skim}/bin/sk --color=bw";
+  file = {
+    ".vimrc" = { source = "${dotfiles}/minimal/.vimrc"; };
+    ".tmux.conf" = { source = "${dotfiles}/minimal/.tmux.conf"; };
+    ".tmuxline.conf" = { source = "${dotfiles}/minimal/.tmuxline.conf"; };
+  } // (if builtins.currentSystem == "x86_64-darwin" then emacs-files else { });
+  sk = "${pkgs.skim}/bin/sk";
   tmux = "${pkgs.tmux}/bin/tmux";
   emacs = "${pkgs.emacs}/bin/emacs";
   emacsclient = "${pkgs.emacs}/bin/emacslient";
   shellAliases = {
     inherit sk;
-    tm = "${tmux} new-session -A -s $(basename $PWD | tr '.' '-') ${emacs}";
+    tm = "${tmux} new-session -A -s $(basename $PWD | tr '.' '-') ${
+        if builtins.currentSystem == "x86_64-darwin" then emacs else ""
+      }";
     tml = "${tmux} list-sessions";
     tma = "${tmux} attach-session -t";
     em = "${emacsclient} -t";
     lsa = "ls -lsa";
     vi = "nvim";
   };
-  skim-files = "${pkgs.fd}/bin/fd '.*' '.' --hidden -E '.git*' | ${sk}";
-  skim-history = ''
-    history | ${sk} | ${pkgs.gawk}/bin/awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}'
-  '';
-  zsh = {
-    enable = true;
-    inherit shellAliases;
-    initExtra = ''
-      restart-nix-daemon () {
-          sudo launchctl bootout system/org.nixos.nix-daemon \
-              && sudo launchctl load /Library/LaunchDaemons/org.nixos.nix-daemon.plist
-      }
-
-      # ----- Keybindings -----
-      __skim_history () {
-          LBUFFER="$LBUFFER$(${skim-history})"
-      }
-      zle -N skim_history __skim_history
-      __skim_files () {
-          LBUFFER="$LBUFFER$(${skim-files})"
-      }
-      zle -N skim_files __skim_files
-      bindkey '^r' skim_history
-      bindkey '^t' skim_files
-
-      # ----- Direnv -----
-      eval "$(${pkgs.direnv}/bin/direnv hook zsh)"
-    '';
-  };
-  bash = {
-    enable = true;
-    inherit shellAliases;
-  };
-  shell-program = if builtins.currentSystem == "x86_64-darwin" then {
-    inherit zsh;
-  } else {
-    inherit bash;
-  };
-  programs = shell-program // {
+  sessionVariables = { SKIM_DEFAULT_OPTIONS = "-m --color=bw"; };
+  programs = {
     direnv = { enable = true; };
+    zsh = {
+      enable = builtins.currentSystem == "x86_64-darwin";
+      inherit shellAliases sessionVariables;
+      initExtra = let
+        skim-files = "${pkgs.fd}/bin/fd '.*' '.' --hidden -E '.git*' | ${sk}";
+        skim-history = ''
+          history | ${sk} | ${pkgs.gawk}/bin/awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}'
+        '';
+      in ''
+        restart-nix-daemon () {
+            sudo launchctl bootout system/org.nixos.nix-daemon \
+                && sudo launchctl load /Library/LaunchDaemons/org.nixos.nix-daemon.plist
+        }
+
+        # ----- Keybindings -----
+        __skim_history () { LBUFFER="$LBUFFER$(${skim-history})" }
+        zle -N skim_history __skim_history
+        __skim_files () { LBUFFER="$LBUFFER$(${skim-files})" }
+        zle -N skim_files __skim_files
+        bindkey '^r' skim_history
+        bindkey '^t' skim_files
+      '';
+    };
+    bash = {
+      enable = builtins.currentSystem == "x86_64-linux";
+      inherit shellAliases sessionVariables;
+    };
     emacs = {
-      enable = true;
+      enable = builtins.currentSystem == "x86_64-darwin";
       package = pkgs.my-emacs;
     };
     gpg = { enable = true; };
--- shell.nix
+++ shell.nix
@@ -1,4 +1,9 @@
-{ pkgs ? import ./pin.nix }: pkgs.mkShell {
+{ pkgs ? import ./pin.nix }:
+pkgs.mkShell {
   name = "dotfiles-shell";
-  packages = with pkgs; [ nixfmt home-manager ];
+  packages = with pkgs;
+    [ nixfmt ] ++ (if builtins.currentSystem == "x86_64-darwin" then
+      [ home-manager ]
+    else
+      [ ]);
 }
