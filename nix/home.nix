09e89fd901ae3af77e50595319a80a6943551836
John Soo <jsoo1@asu.edu>
2021-10-19 07:21:08 -0700

    nix: Get shellcheck.

--- nix/home.nix
+++ nix/home.nix
@@ -83,7 +83,7 @@ in lib.optionalAttrs (!isDarwin) { inherit systemd services; } // {
       inherit (pkgs)
         bashCompletion bashInteractive dogdns fd gawk gdb ghcid git
         haskell-language-server iosevka libressl nix-diff nix-prefetch nixfmt
-        rage restream ripgrep rnix-lsp rr socat terraform-lsp watch;
+        rage restream ripgrep rnix-lsp rr shellcheck socat terraform-lsp watch;
       fonts = [ iosevka ];
       haskell-utilities = [ ghcid haskell-language-server ];
       c-utilities = [ gdb ] ++ lib.optional (!isDarwin) rr;
@@ -91,7 +91,7 @@ in lib.optionalAttrs (!isDarwin) { inherit systemd services; } // {
       nix-utilities = [ nixfmt nix-diff nix-prefetch rnix-lsp ];
       remarkable-utilities = [ restream ];
       shell-utilities =
-        [ bashCompletion dogdns fd gawk git rage ripgrep watch ];
+        [ bashCompletion dogdns fd gawk git rage ripgrep shellcheck watch ];
       socket-utilities = [
         libressl # see "nc" in extraOutputsToInstall
         socat
