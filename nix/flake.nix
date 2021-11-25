5b023a70a8d4147a62cbfeb14c996858340af59d
John Soo <jsoo1@asu.edu>
2021-11-24 19:01:34 -0800

    nix: Add pkgs to flake outputs for repl convenience.

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,6 +1,7 @@
 {
   description = "A home-manager/nix-darwin configuration";
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url =
       "github:NixOS/nixpkgs/67e5945d357ffa2d9bf7aa40fa59ddfd99513f12";
     home-manager = {
@@ -13,49 +14,62 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, home-manager, darwin }: rec {
-    defaultPackage.x86_64-linux =
-      (import nixpkgs { system = "x86_64-linux"; }).writeShellScriptBin
-        "home-manager" ''
-        ${home-manager.defaultPackage.x86_64-linux}/bin/home-manager switch --flake .#homeConfigurations.john
-      '';
-
-    defaultPackage.x86_64-darwin =
-      let
-        system = "x86_64-darwin";
-        pkgs = import nixpkgs { inherit system; };
-      in
-      pkgs.writeShellScriptBin "darwin-rebuild" ''
-        ${darwinConfigurations.johhsoo.system}/sw/bin/darwin-rebuild --impure --flake .# "$@"
-      '';
-
-    darwinConfigurations.johhsoo = darwin.lib.darwinSystem
-      {
-        system = "x86_64-darwin";
-        modules = [
-          ({ pkgs, ... }: {
-            nixpkgs.overlays = import ./my-emacs.nix ++ import ./restream.nix;
-          })
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.users."johh.soo" = import ./home.nix;
-          }
-          ./darwin.nix
-        ];
-      };
-
-    homeConfigurations.john =
-      let
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; overlays = import ./my-emacs.nix ++ import ./restream.nix; };
-      in
-      home-manager.lib.homeManagerConfiguration
-        {
-          username = "john";
-          homeDirectory = "/home/john";
-          configuration.imports = [ ./home.nix ];
-          inputs = { inherit home-manager; };
+  outputs = { self, nixpkgs, flake-utils, home-manager, darwin }:
+    let
+      overlays = import ./my-emacs.nix ++ import ./restream.nix;
+    in
+    rec {
+      # Useful for the following nix repl scenario to replace `nix repl pin.nix`:
+      # $ nix repl
+      # > fl = builtins.getFlake "*this $PWD here*"
+      # > :b fl.pkgs.x86_64-darwin.ncurses
+      packages = flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system: {
+        pkgs = import nixpkgs {
+          inherit system overlays;
         };
-  };
+      });
+
+      defaultPackage.x86_64-linux =
+        (import nixpkgs { system = "x86_64-linux"; }).writeShellScriptBin
+          "home-manager" ''
+          ${home-manager.defaultPackage.x86_64-linux}/bin/home-manager switch --flake .#homeConfigurations.john
+        '';
+
+      defaultPackage.x86_64-darwin =
+        let
+          system = "x86_64-darwin";
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.writeShellScriptBin "darwin-rebuild" ''
+          ${darwinConfigurations.johhsoo.system}/sw/bin/darwin-rebuild --impure --flake .# "$@"
+        '';
+
+      darwinConfigurations.johhsoo = darwin.lib.darwinSystem
+        {
+          system = "x86_64-darwin";
+          modules = [
+            ({ pkgs, ... }: {
+              nixpkgs = { inherit overlays; };
+            })
+            home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.users."johh.soo" = import ./home.nix;
+            }
+            ./darwin.nix
+          ];
+        };
+
+      homeConfigurations.john =
+        let
+          system = "x86_64-linux";
+          pkgs = import nixpkgs { inherit system overlays; };
+        in
+        home-manager.lib.homeManagerConfiguration
+          {
+            username = "john";
+            homeDirectory = "/home/john";
+            configuration.imports = [ ./home.nix ];
+          };
+    };
 }
