18a659599f6a22f8b536893d22a66e3eb5797c19
John Soo <jsoo1@asu.edu>
2021-11-25 02:04:10 -0800

    nix: Setup nix-shell and fix homeConfiguration.

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,6 +2,10 @@
   description = "A home-manager/nix-darwin configuration";
   inputs = {
     dotfiles.url = "git+https://git.sr.ht/~jsoo/dotfiles?ref=release";
+    flake-compat = {
+      flake = false;
+      url = "github:edolstra/flake-compat";
+    };
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url =
       "github:NixOS/nixpkgs/67e5945d357ffa2d9bf7aa40fa59ddfd99513f12";
@@ -15,23 +19,32 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, dotfiles, nixpkgs, flake-utils, home-manager, darwin }:
+  outputs = { self, dotfiles, nixpkgs, flake-utils, home-manager, darwin, ... }:
     let
       overlays = import ./my-emacs.nix ++ import ./restream.nix;
+      devThings = flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
+        (system:
+          let pkgs = import nixpkgs { inherit system overlays; }; in
+          {
+            # Useful for the following nix repl scenario to replace `nix repl pin.nix`:
+            # $ nix repl
+            # > fl = builtins.getFlake "*this $PWD here*"
+            # > :b fl.pkgs.x86_64-darwin.ncurses
+
+            packages = pkgs;
+            devShell = pkgs.mkShell {
+              buildInputs = [ pkgs.home-manager ];
+            };
+          }
+        );
     in
-    rec {
-      # Useful for the following nix repl scenario to replace `nix repl pin.nix`:
-      # $ nix repl
-      # > fl = builtins.getFlake "*this $PWD here*"
-      # > :b fl.pkgs.x86_64-darwin.ncurses
-      packages = flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system: {
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-      });
+    {
+      inherit (devThings) packages devShell;
 
       defaultPackage.x86_64-linux =
-        (import nixpkgs { system = "x86_64-linux"; }).writeShellScriptBin
+        (import nixpkgs {
+          system = "x86_64-linux";
+        }).writeShellScriptBin
           "home-manager" ''
           ${home-manager.defaultPackage.x86_64-linux}/bin/home-manager switch --flake .#homeConfigurations.john
         '';
@@ -60,6 +73,7 @@
         in
         home-manager.lib.homeManagerConfiguration
           {
+            inherit system;
             username = "john";
             homeDirectory = "/home/john";
             configuration.imports = [ ./home.nix ];
---
+++ nix/shell.nix
@@ -0,0 +1,9 @@
+let
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  flake-compat = fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
+  };
+  flake = import flake-compat { src = ./.; };
+in
+flake.shellNix
