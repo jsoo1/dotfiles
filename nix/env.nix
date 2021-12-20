9d875146e82e951dbec3d5c87cd6c681293f5eb7
John Soo <jsoo1@asu.edu>
2021-12-20 07:58:49 -0800

    nix: Add coreutils to env.

--- nix/env.nix
+++ nix/env.nix
@@ -3,10 +3,11 @@
 }:
 let
   inherit (pkgs)
-    bashCompletion bashInteractive bottom dogdns du-dust exa fd gawk gdb ghcid git
-    haskell-language-server iosevka less libressl neovim nix-diff
-    nix-prefetch nixpkgs-fmt peep rage recutils restream ripgrep rnix-lsp rr shellcheck socat
-    tealdeer terraform-lsp watch;
+    bashCompletion bashInteractive bottom coreutils dogdns
+    du-dust exa fd gawk gdb ghcid git haskell-language-server
+    iosevka less libressl neovim nix-diff nix-prefetch
+    nixpkgs-fmt peep rage recutils restream ripgrep rnix-lsp
+    rr shellcheck socat tealdeer tee terraform-lsp watch;
 
   haskell-utilities = [ ghcid haskell-language-server ];
 
@@ -21,6 +22,7 @@ let
   shell-utilities = [
     bashCompletion
     bottom
+    coreutils
     dogdns
     du-dust
     exa
