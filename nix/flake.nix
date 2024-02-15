d6aea4ebc590511df2ee787b05cdb7737fe18e89
John Soo <jsoo1@asu.edu>
2024-02-15 11:06:40 -0700

    nix: Bump nixpkgs, home-manager.

--- nix/flake.nix
+++ nix/flake.nix
@@ -15,7 +15,7 @@
       url = "github:edolstra/flake-compat";
     };
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:jsoo1/nixpkgs/release-2023-09-24";
+    nixpkgs.url = "github:jsoo1/nixpkgs/release-2024-02-15";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
