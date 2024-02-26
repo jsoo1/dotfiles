74b09434c3100d32ed70b0d1f635147e6cae2257
John Soo <jsoo1@asu.edu>
2024-02-25 21:51:33 -0700

    vm: Bring it up to the times a bit

--- nix/vbox.nix
+++ nix/vbox.nix
@@ -39,7 +39,7 @@
     config.init.defaultBranch = "main";
   };
 
-  time.timeZone = "America/Los_Angeles";
+  time.timeZone = "America/Denver";
 
   services.openssh = {
     enable = true;
@@ -79,6 +79,8 @@
     group = "users";
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDKY253LDGAf14dHxXlhh2BPj9hvfQMcEMlv2z3Q0zg/HVJGwT1qC0pwbhS5DBckqYs2DzNmoJ0AP9KRmsMZJtbal/Vzffe/MXEwK852u4a2DenxIohFY1La+JLVkX0NGpvOTOkIN1ScZ3UzIpJEkcJniHT+RfNmHtJUJTb/XXS2dULiqby2JpIS5BpqLURTmXeoQQ7fZsGaMDwVtI53PhcUaC6Fz9dAMS5GiK6xlLguIHqqa9tojQYwM4NLIrr1vHGrt3MW2ODtTuHePyRrBvfD98Lnr0nu3syQfgNxFOpkxx0FP7TGY1s6t2wwXDdlevJThxjC3utHTsQBSbcKWajKyAsHnsjQqZmF5802EO+iYwuSH8zhXb4+X/ieKkg9NSPCbeaiI/fTMs3bsJOruPcrjDFjLnzxRmgFW0V+nLFZJLunnXv5JiKvXkepC2ITJEVfwDZsucCUDG81+JMq0yqmPd9PY/hERW1AhKoRnnI6Qih9ddwVBrs9PSf41bcyW4cfO3FGtqDUbod73idVwCvzN58GTttnRhC1hvUajVS3u1xpWxYRw3vz/y7x7UPuNWVCGDuAd2yNh9f08LlDweyKjqh8gMRUA6oy5zQPlE3vc32AAW84SccuUYXu+AqY+ZcL4RytHU1XCru4bP1WUXnRws6MtvM010kq9U9AQwr4w== johh.soo@johhsoo" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILK6/O2/x73878Nz0Jy4nhL8A4lJqH+G43oOZI2yejB4 cardno:18_556_863"
+    ];
   };
 }
