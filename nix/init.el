1ed4949ebe71eb408e1acddeb281b1e5b88de53f
John Soo <jsoo1@asu.edu>
2024-05-17 13:38:29 -0600

    nix: Python emacs stuff

--- nix/init.el
+++ nix/init.el
@@ -942,6 +942,43 @@ Take newline delimited `STRING' and return list of all
      "C-c C-z" 'nodejs-repl-switch-to-repl)))
 (setq js-indent-level 4)
 
+;; Python
+(require 'flycheck)
+(flycheck-define-checker
+    python-mypy ""
+    :command ("mypy" source-original)
+    :error-patterns
+    ((error line-start (file-name) ":" line ": error:" (message) line-end))
+    :modes python-mode)
+
+(add-to-list 'flycheck-checkers 'python-mypy t)
+
+;; From: https://gist.github.com/abo-abo/277d1fe1e86f0e46d3161345f26e8f3a
+(flycheck-define-checker python-ruff
+  "A Python syntax and style checker using the ruff utility.
+To override the path to the ruff executable, set
+`flycheck-python-ruff-executable'.
+See URL `http://pypi.python.org/pypi/ruff'."
+  :command ("ruff"
+            "check"
+            "--outputt-format=concise"
+            (eval (when buffer-file-name
+                    (concat "--stdin-filename=" buffer-file-name)))
+            "-")
+  :standard-input t
+  :error-filter (lambda (errors)
+                  (let ((errors (flycheck-sanitize-errors errors)))
+                    (seq-map #'flycheck-flake8-fix-error-level errors)))
+  :error-patterns
+  ((warning line-start
+            (file-name) ":" line ":" (optional column ":") " "
+            (id (one-or-more (any alpha)) (one-or-more digit)) " "
+            (message (one-or-more not-newline))
+            line-end))
+  :modes python-mode)
+
+(add-to-list 'flycheck-checkers 'python-ruff t)
+
 ;; Json
 (require 'json-ts-mode)
 (add-to-list 'auto-mode-alist '("\\.json\\'" . json-ts-mode))
